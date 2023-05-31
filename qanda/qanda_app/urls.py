7510c09c983a02b293d057ea560c10703a564d9b
PorridgePower <rock-with-me@yandex.ru>
2023-06-01 00:27:53 +0300

    Add questions to index page

--- qanda/qanda_app/urls.py
+++ qanda/qanda_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("",
+        view=views.QuestionsView.as_view(), name="index"),
     path('<int:question_id>/', views.question_detail, name='detail'),
 ]
--- qanda/qanda_app/views.py
+++ qanda/qanda_app/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView
 from django.http import Http404
 from django.http import HttpResponse
 from .models import Question
 
 
+class QuestionsView(ListView):
+    template_name = "index.html"
+    context_object_name = 'questions'
+    def get_queryset(self, *args, **kwargs):
+        latest_question_list = Question.objects.order_by('-pub_date')[:5]
+        return latest_question_list
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([q.content for q in latest_question_list])
-    return render(request,'index.html')
+    return render(latest_question_list,'index.html')
 
 def question_detail(request, question_id):
     try:
